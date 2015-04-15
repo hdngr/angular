029573dca3fc58a83ddcdbf6b8cb27b3e1edf497
Misko Hevery <misko@hevery.com>
2015-04-15 22:35:38 +0000

    docs: add DI to public docs

--- modules/angular2/src/di/key.js
+++ modules/angular2/src/di/key.js
@@ -2,6 +2,18 @@ import {KeyMetadataError} from './exceptions';
 import {MapWrapper, Map} from 'angular2/src/facade/collection';
 import {int, isPresent} from 'angular2/src/facade/lang';
 
+/**
+ * A unique object used for retrieving items from the Injector.
+ *
+ * [Key]s have:
+ * - system wide unique [id].
+ * - [token] usually the [Type] of the instance.
+ *
+ * [Key]s are used internaly in [Injector] becouse they have system wide unique [id]s which allow the injector to
+ * index in arrays rather ther look up items in maps.
+ *
+ * @exportedAs angular2/di
+ */
 export class Key {
   token;
   id:int;
@@ -12,23 +24,24 @@ export class Key {
     this.metadata = null;
   }
 
-  static setMetadata(key:Key, metadata):Key {
-    if (isPresent(key.metadata) && key.metadata !== metadata) {
-      throw new KeyMetadataError();
-    }
-    key.metadata = metadata;
-    return key;
-  }
-
+  /**
+   * Retrieve a [Key] for a token.
+   */
   static get(token):Key {
     return _globalKeyRegistry.get(token);
   }
 
+  /**
+   * @returns number of [Key]s registered in the system.
+   */
   static get numberOfKeys():int {
     return _globalKeyRegistry.numberOfKeys;
   }
 }
 
+/**
+ * @private
+ */
 export class KeyRegistry {
   _allKeys:Map;
   constructor() {
